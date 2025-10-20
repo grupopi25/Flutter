78ac93ee433575fa35a047d30dd11551dbac1c8b
grupopi25 <grupopiii27@gmail.com>
2025-10-20 17:36:00 -0300

    iniciando o codigo

---
+++ gerador_de_tabuada/lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:gerador_de_tabuada/home.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Home(),
+        ),
+      ),
+    );
+  }
+}
